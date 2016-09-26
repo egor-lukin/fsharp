30c07f07e301a9b0149d2d521cd6eff41845d841
LukinEgor <e.lukin@simcase.ru>
2016-09-26 09:26:46 +0400

    1 labs 8 variant

--- lab1/program.fs
+++ lab1/program.fs
@@ -1,5 +1,15 @@
+//8. Для заданного списка слов найти слова, не содержащие буквы Е
+
+open System
+
 [<EntryPoint>]
 let main argv =
-  printfn "hello world"
-  0
 
+  let symbol = argv.[0]
+
+  argv.[1..]
+    |> Array.toList
+    |> List.filter (fun word -> String.forall (fun c -> Convert.ToString(c) <> symbol) word)
+    |> List.iter (fun word -> Console.WriteLine word)
+
+  0;;
